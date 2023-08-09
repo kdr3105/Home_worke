4eca88310abe968008906457a92d21cebcbb770e
kdr3105 <kirpichovdamer@yandex.ru>
2023-08-09 13:52:58 +0300

    Проработка домашнего задания и файл json

--- dz10_1.py
+++ dz10_1.py
@@ -18,28 +18,41 @@ def page_index():
     print_data += '</pre>'
     return print_data
 @app.route("/candidate/<int:xid>")
-def pagemessages():
+def pagemessages(xid):
     print_candidat = ''
+    exist_candidate = True
     for i in my_candidats:
         if i['id']==xid:
-            print_candidat = f'< img src = "({i["picture"]})">\n'
-            print_candidat += f"<рге>"
+            print_candidat = f'<img src="({i["picture"]})">\n'
+            print_candidat += f"</img>\n"
+            print_candidat += f"<рге>\n"
             print_candidat += f"Имя кандидата - {i['name']}\n"
             print_candidat += f"Позиция кандидата {i['position']}\n"
             print_candidat += f"Навыки через запятую {i['skills']}\n"
-            print_candidat += f"</рге>"
+            print_candidat += f"</рге>\n"
+            exist_candidate = False
             break
-        else:
-            print_candidat += f"<рге>"
-            print_candidat += f"Нет такого кандидата"
-            print_candidat += f"</рге>"
-    return "Сообщения пользователя"
+    if exist_candidate:
+        print_candidat += f"<рге>\n"
+        print_candidat += f"Нет такого кандидата\n"
+        print_candidat += f"</рге>\n"
+    return print_candidat
 
-@app.route('/users/<uid>')
-def profile(uid):
-    return f'<h1>Профиль {uid}</h1>'
-@app.route('/catalog/items/<itemid>')
-def profile_1(itemid):
-    return f'<h1>Страничка товара {itemid}</hl>'
+@app.route('/skill/<string:scill_x>')
+def skill_finde(scill_x):
+    skill_data = ''
+    skill_data += '<pre>\n'
+    exist_skils = True
+    for i in my_candidats:
+        if str(scill_x).lower() in str(i["skills"]).lower().split(', '):
+            skill_data += f"Имя кандидата - {i['name']}\n"
+            skill_data += f"Позиция кандидата {i['position']}\n"
+            skill_data += f"Навыки через запятую {i['skills']}\n"
+            skill_data += f"=========================================================\n"
+            exist_skils = False
+    if exist_skils:
+        skill_data += "Нет кандидатов с таким навыком\n"
+    skill_data += '</pre>\n'
+    return skill_data
 
 app.run()
